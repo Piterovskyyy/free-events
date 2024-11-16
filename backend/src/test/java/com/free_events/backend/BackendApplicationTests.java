d21a2dcf490dad74d8c98def55e029cb0e806b4a
janekbartnicki <jan.bartnicki01@gmail.com>
2024-11-16 12:20:52 +0100

    Backend init

---
+++ backend/src/test/java/com/free_events/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free_events.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
